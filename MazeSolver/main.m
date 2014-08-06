a1330796d9fd281ced4e90f30dee7869fcdddc65
Kenner <kennerminer@gmail.com>
2014-08-06 07:40:32 -0700

    Initial Commit

---
+++ MazeSolver/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MazeSolver
+//
+//  Created by Kenner Miner on 8/6/14.
+//  Copyright (c) 2014 Kenner Miner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
